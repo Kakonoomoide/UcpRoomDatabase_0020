3834560359508770fc7b286ce1df255067e73b55
Kakonoomoide <rizkiramada128@gmail.com>
2024-12-18 17:47:48 +0700

    ADD - methode untuk insert data dokter dan read data dokter

--- app/src/main/java/com/example/pam_ucp2/repository/RepositoryDktr.kt
+++ app/src/main/java/com/example/pam_ucp2/repository/RepositoryDktr.kt
@@ -1,4 +1,12 @@
 package com.example.pam_pert9.repository
 
+import com.example.pam_ucp2.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryDktr {
+    suspend fun insertDktr(dokter: Dokter)
+
+    fun getAllDktr(): Flow<List<Dokter>>
+
+    fun getDktr(idDktr: String): Flow<Dokter>
 }
