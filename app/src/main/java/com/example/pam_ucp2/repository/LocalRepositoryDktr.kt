05844e2e16785d53d26c571ff6581a1797d9decc
Kakonoomoide <rizkiramada128@gmail.com>
2024-12-18 17:53:08 +0700

    ADD - methode untuk insert dan read data dokter yang telah di buat di dao

--- app/src/main/java/com/example/pam_ucp2/repository/LocalRepositoryDktr.kt
+++ app/src/main/java/com/example/pam_ucp2/repository/LocalRepositoryDktr.kt
@@ -1,4 +1,20 @@
 package com.example.pam_pert9.repository
 
-class LocalRepositoryDktr() : RepositoryDktr {
+import com.example.pam_ucp2.data.dao.DokterDao
+import com.example.pam_ucp2.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryDktr(private val dokterDao:DokterDao) : RepositoryDktr {
+    override suspend fun insertDktr(dokter: Dokter) {
+        dokterDao.insertDokter(dokter)
+    }
+
+    override fun getAllDktr(): Flow<List<Dokter>> {
+        return dokterDao.getAllDokter()
+    }
+
+    override fun getDktr(idDktr: String): Flow<Dokter> {
+        return dokterDao.getDokter(idDktr)
+    }
+
 }
