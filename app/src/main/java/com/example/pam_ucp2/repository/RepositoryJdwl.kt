da8629bc1a4afae4587cc509325ddbc08968115d
Kakonoomoide <rizkiramada128@gmail.com>
2024-12-18 17:55:40 +0700

    ADD - methode untuk insert dan read data jadwal

--- app/src/main/java/com/example/pam_ucp2/repository/RepositoryJdwl.kt
+++ app/src/main/java/com/example/pam_ucp2/repository/RepositoryJdwl.kt
@@ -1,4 +1,16 @@
 package com.example.pam_ucp2.repository
 
+import com.example.pam_ucp2.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryJdwl {
+    suspend fun insertJdwl(jadwal: Jadwal)
+
+    suspend fun deleteJdwl(jadwal: Jadwal)
+
+    suspend fun updateJdwl(jadwal: Jadwal)
+
+    fun getAllJdwl(): Flow<List<Jadwal>>
+
+    fun getJdwl(nim: String): Flow<Jadwal>
 }
