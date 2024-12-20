85c78d9508432a11cd88c63e104d413e1d0b92de
Kakonoomoide <rizkiramada128@gmail.com>
2024-12-20 21:05:17 +0700

    ADD - methode untuk create delete dan update jadwal

--- app/src/main/java/com/example/pam_ucp2/repository/LocalRepositoryJdwl.kt
+++ app/src/main/java/com/example/pam_ucp2/repository/LocalRepositoryJdwl.kt
@@ -1,4 +1,20 @@
 package com.example.pam_ucp2.repository
 
-class LocalRepositoryJdwl() : RepositoryJdwl {
+import com.example.pam_ucp2.data.dao.JadwalDao
+import com.example.pam_ucp2.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryJdwl(private val jadwalDao: JadwalDao) : RepositoryJdwl {
+    override suspend fun insertJdwl(jadwal: Jadwal) {
+        jadwalDao.insertJadwal(jadwal)
+    }
+
+    override suspend fun deleteJdwl(jadwal: Jadwal) {
+        jadwalDao.deleteJadwal(jadwal)
+    }
+
+    override suspend fun updateJdwl(jadwal: Jadwal) {
+        jadwalDao.updateJadwal(jadwal)
+    }
+
 }
