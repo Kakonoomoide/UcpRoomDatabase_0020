954aaacc43948acca486ed067c54c7c802284569
Kakonoomoide <rizkiramada128@gmail.com>
2024-12-20 23:23:54 +0700

    ADD - Composable InsertDktr untuk tampilan form tambah dokter dengan snackbar dan navigasi

--- app/src/main/java/com/example/pam_ucp2/ui/view/dokter/InsertDktr.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/view/dokter/InsertDktr.kt
@@ -3,19 +3,33 @@ package com.example.pam_ucp2.ui.view.dokter
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam_ucp2.ui.costumewiget.TopAppBar
 import com.example.pam_ucp2.ui.navigation.AlamatNavigasi
+import com.example.pam_ucp2.ui.viewmodel.PenyediaViewModel
 import com.example.pam_ucp2.ui.viewmodel.dokter.DktrUIState
 import com.example.pam_ucp2.ui.viewmodel.dokter.DokterEvent
+import com.example.pam_ucp2.ui.viewmodel.dokter.DokterViewModel
 import com.example.pam_ucp2.ui.viewmodel.dokter.FormErrorState
+import kotlinx.coroutines.launch
 
 object DestinasiInsert : AlamatNavigasi {
     override val route: String = "insert_dktr"
@@ -23,9 +37,57 @@ object DestinasiInsert : AlamatNavigasi {
 
 @Composable
 fun InsertDktr(
-
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DokterViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+// get uistate and snackbar
+    val uiState = viewModel.uiState
+    val snacBarHostState = remember{SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
+    // obser on change snack bar
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                //tampil snackbar
+                snacBarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snacBarHostState) }
+    ){ padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Dokter"
+            )
+            // isi body
+            InsertBodyDktr (
+                uiState = uiState,
+                onValueChange = { uppdateEvent ->
+                    viewModel.updateState(uppdateEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        viewModel.saveData()
+                    }
+                    onNavigate()
+                }
+            )
+        }
+    }
 }
 
 @Composable
