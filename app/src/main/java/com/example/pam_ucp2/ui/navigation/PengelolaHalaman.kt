345f43cba25cebe8007226fe1e44e5e34b968db8
Kakonoomoide <rizkiramada128@gmail.com>
2024-12-21 21:27:12 +0700

    ADD - pengelola halaman dengan NavHost untuk navigasi antar halaman

--- app/src/main/java/com/example/pam_ucp2/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/navigation/PengelolaHalaman.kt
@@ -1,2 +1,30 @@
 package com.example.pam_ucp2.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pam_ucp2.ui.view.dokter.DestinasiInsert
+import com.example.pam_ucp2.ui.view.dokter.InsertDktr
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiInsert.route
+    ) {
+        composable(
+            route = DestinasiInsert.route
+        ){
+            InsertDktr(
+                onBack = {},
+                onNavigate = {}
+            )
+        }
+    }
+}
