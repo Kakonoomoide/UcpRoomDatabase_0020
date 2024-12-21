548f8a905440a8b9d444aaf6ed2874a836e90204
Kakonoomoide <rizkiramada128@gmail.com>
2024-12-21 21:29:57 +0700

    ADD - peneglola halaman ke dalam main activity

--- app/src/main/java/com/example/pam_ucp2/MainActivity.kt
+++ app/src/main/java/com/example/pam_ucp2/MainActivity.kt
@@ -1,4 +1,4 @@
-    package com.example.pam_ucp2
+package com.example.pam_ucp2
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pam_ucp2.ui.navigation.PengelolaHalaman
 import com.example.pam_ucp2.ui.theme.PAMUCP2Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PAMUCP2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
