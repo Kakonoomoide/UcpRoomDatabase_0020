3fff0cea0bd85a46b962a2490feeb6008740a923
Kakonoomoide <rizkiramada128@gmail.com>
2024-12-22 20:34:19 +0700

    ADD - inisialisasi ViewModel untuk HomeDktr dan Jadwal dengan repository terkait

--- app/src/main/java/com/example/pam_ucp2/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam_ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -6,12 +6,24 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pam_ucp2.AdminRsApp
 import com.example.pam_ucp2.ui.viewmodel.dokter.DokterViewModel
+import com.example.pam_ucp2.ui.viewmodel.dokter.HomeDktrViewModel
+import com.example.pam_ucp2.ui.viewmodel.jadwal.JadwalViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer {
             DokterViewModel(
                 adminRsApp().containerApp.repositoryDktr
+            )
+        }
+        initializer {
+            HomeDktrViewModel(
+                adminRsApp().containerApp.repositoryDktr
+            )
+        }
+        initializer {
+            JadwalViewModel(
+                adminRsApp().containerApp.repositoryJdwl
             )
         }
     }
